9d414a1d94831674e67c86d1accd44b2fa169f28
Yolanda <yeckdemies@gmail.com>
2024-04-29 13:58:59 +0200

    Ejercicio 4

--- ejercicio-4.js
+++ ejercicio-4.js
@@ -5,6 +5,7 @@ const users = [
   { id: 3, name: 'Pedro' },
   { id: 4, name: 'Amanda' }
 ];
+const names = users.map((user) => user.name);
 
 /* 4.2 Dado el siguiente array, devuelve una lista que contenga los valores
 de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que
@@ -16,6 +17,13 @@ const users2 = [
   { id: 4, name: 'Amanda' }
 ];
 
+const namesNew = users2.map((user) => {
+  if (user.name[0] === 'A') {
+    user.name = 'Anacleto';
+  }
+  return user.name;
+});
+
 /* 4.3 Dado el siguiente array, devuelve una lista que contenga los valores
 de la propiedad .name y añade al valor de .name el string ' (Visitado)'
 cuando el valor de la propiedad isVisited = true. */
@@ -25,3 +33,12 @@ const cities = [
   { isVisited: true, name: 'Amsterdam' },
   { isVisited: false, name: 'Seul' }
 ];
+
+const visitedCities = cities.map((city) => {
+  if (city.isVisited) {
+    city.name = city.name + ' (Visitado)';
+  }
+  return city.name;
+});
+
+console.log(visitedCities);
